cd1aecf068cddf165cb64e1d264be44e72247f14
Hazzanawarafik <hazzanawa@gmail.com>
2024-01-11 20:12:52 +0700

    Menjalankan Event Pada Inline Element

--- selection/app.js
+++ selection/app.js
@@ -1,12 +1,3 @@
-// cari element dengan nama id 
-//document.getElementById('id')
-
-// cari element dengan nama tag 
-// document.getElementByTagName('img')
-
-// cari element dengan nama class
-// document.getElementByClassname('btn btn-primary')
-
 // const allImages = document.getElementsByTagName('img');
 
 // for (let img of allImages) {
@@ -22,38 +13,10 @@
 // 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
 // }
 
+const links = document.querySelectorAll('a');
 
-
-
-///////////////////////////////////////////////////////////////////////// querySelector
-// cari element h1 pertama
-// document.querySelector('h1')
-
-// cari element pertama dengan id bernilai "red"
-// document.querySelector('#red')
-
-//cari element pertama dengan class bernilai "big"
-// document.querySelector('.big')
-// const links = document.querySelectorAll('p a');
-
-//cari element button dengan type 
-//document.querySelector('button[type="checkbox"]')
-
-//cari element dengan input type
-// document.querySelector('input[type="checkbox"]')
-
-// cari element dengan div lang
-// document.querySelector('div[lang="en"]')
-
-
-// for(let link of links){
-// 	console.log(link.href);
-// }
-// for (let link of links) {
-// 	link.style.color = 'rgb(0, 108, 134)';
-// 	link.style.textDecorationColor = 'magenta';
-// 	link.style.textDecorationStyle = 'wavy';
-// }
-
-
-
+for (let link of links) {
+	link.style.color = 'rgb(0, 108, 134)';
+	link.style.textDecorationColor = 'magenta';
+	link.style.textDecorationStyle = 'wavy';
+}
