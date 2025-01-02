20d553065af6c7593f538c701a3bd164223e448f
Hazzanawarafik <hazzanawa@gmail.com>
2025-01-02 23:56:34 +0700

    Event input dan change pada form

--- event/app.js
+++ event/app.js
@@ -2,6 +2,16 @@ const form = document.querySelector('#form');
 const list = document.querySelector('#notes');
 const input = document.querySelector('input');
 
+
+// input.addEventListener('change',(e) =>{
+// 	console.log('Nilai Berubah');
+// });
+
+input.addEventListener('input',(e) =>{
+	document.querySelector('h1').innerText = input.value;
+	console.log('Nilai Berhasil diiinput');
+});
+
 form.addEventListener('submit', function(e){
 	e.preventDefault();
 	const noteValue = input.value;
